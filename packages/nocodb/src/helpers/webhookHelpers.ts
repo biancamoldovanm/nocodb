e5bca70719b4c481bacefdf4ccc9382f5799aeb3
Fendy Heryanto <fendy@nocodb.com>
2025-03-10 02:26:18 +0000

    remove console.log

--- packages/nocodb/src/helpers/webhookHelpers.ts
+++ packages/nocodb/src/helpers/webhookHelpers.ts
@@ -986,7 +986,6 @@ export async function getAffectedColumns(
     return undefined;
   }
   const compareSingle = (prev, next) => {
-    console.log(prev, next);
     const updatePayload = populateUpdatePayloadDiff({
       prev,
       next,
